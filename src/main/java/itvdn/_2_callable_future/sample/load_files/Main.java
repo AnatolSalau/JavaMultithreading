248f52920b1903e557db58a963fcea6a3bbe6a7e
AnatolSalau <salovanatoly5@mail.ru>
2024-02-17 22:47:48 +0300

    Future Callable

--- src/main/java/itvdn/_2_callable_future/sample/load_files/Main.java
+++ src/main/java/itvdn/_2_callable_future/sample/load_files/Main.java
@@ -1,12 +1,12 @@
 package itvdn._2_callable_future.sample.load_files;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.lang.management.ThreadInfo;
+import java.util.concurrent.*;
 
 public class Main {
-      public static void main(String[] args) {
-            //testSynchronous();
+      private final static int POOL_SIZE = Runtime.getRuntime().availableProcessors();
+      public static void main(String[] args) throws InterruptedException, ExecutionException {
+            testSynchronous();
             testAsynchronous();
       }
 
@@ -21,29 +21,37 @@ public class Main {
             System.out.println("Time for statistic : " + (end - start));
             System.out.println();
       }
-      private static void testAsynchronous() {
+      private static void testAsynchronous() throws InterruptedException, ExecutionException {
+            long start = System.currentTimeMillis();
             Callable<Void> callable1 = () -> {
-                  FileUtils.getStatistic("src/main/resources/result1.csv", "src/main/resources/file1.csv");
+                  FileUtils.getStatistic("src/main/resources/result10.csv", "src/main/resources/file1.csv");
                   return null;
             };
             Callable<Void> callable2 = () -> {
-                  FileUtils.getStatistic("src/main/resources/result2.csv", "src/main/resources/file2.csv");
+                  FileUtils.getStatistic("src/main/resources/result20.csv", "src/main/resources/file2.csv");
                   return null;
             };
             Callable<Void> callable3 = () -> {
-                  FileUtils.getStatistic("src/main/resources/result3.csv", "src/main/resources/file3.csv");
+                  FileUtils.getStatistic("src/main/resources/result30.csv", "src/main/resources/file3.csv");
                   return null;
             };
-            ExecutorService executorService = Executors.newFixedThreadPool(3);
-            long start = System.currentTimeMillis();
+            ExecutorService executorService = Executors.newFixedThreadPool(POOL_SIZE);
 
-            executorService.submit(callable1);
-            executorService.submit(callable2);
-            executorService.submit(callable3);
+            Future<Void> submit = executorService.submit(callable1);
+            Future<Void> submit1 = executorService.submit(callable2);
+            Future<Void> submit2 = executorService.submit(callable3);
+
+            submit.get();
+            submit1.get();
+            submit2.get();
+            if (submit.isDone() && submit1.isDone() && submit2.isDone()) {
+                  System.out.println("All futures completed");
+            }
+            executorService.shutdown();
 
             long end = System.currentTimeMillis();
-            executorService.shutdown();
-            System.out.println("Test asynchronous");
+
+            System.out.println("Test asynchronous, processors : " + POOL_SIZE);
             System.out.println("Time for statistic : " + (end - start));
             System.out.println();
       }
--- src/main/java/itvdn/_2_callable_future/sample/weather/Main.java
+++ src/main/java/itvdn/_2_callable_future/sample/weather/Main.java
@@ -2,8 +2,10 @@ package itvdn._2_callable_future.sample.weather;
 
 import java.io.IOException;
 import java.net.http.HttpClient;
+import java.util.concurrent.*;
 
 public class Main {
+      private final static int POOL_SIZE = Runtime.getRuntime().availableProcessors();
       private static final String OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?lat=53.90&lon=27.55&appid=252c0950663084f347d9b3f79b8f25ba";
       private static final String TOMORROW_WEATHER_URL = "https://api.tomorrow.io/v4/weather/forecast?location=53.9006,27.5590&apikey=Ibadk8a0n9R27L1gXnOTi0Q4f6tsJAuS";
       private static final String WEATHERBIT_WEATHER_URL = "https://api.weatherbit.io/v2.0/current?lat=53.9006&lon=27.5590&key=e1c7e6088ca549458378472403c3a03b&include=minutely";
@@ -12,6 +14,7 @@ public class Main {
 
       public static void main(String[] args) {
             testSynchronous();
+            testAsynchronous();
 
       }
 
@@ -24,7 +27,7 @@ public class Main {
                   long end = System.currentTimeMillis();
                   long duration = end - start;
                   System.out.println("Test 1 synchronous");
-                  System.out.println(duration);
+                  System.out.println("Duration : " + duration);
                   System.out.println();
 
             } catch (IOException | InterruptedException e) {
@@ -33,6 +36,35 @@ public class Main {
       }
 
       private static void testAsynchronous() {
+            try(HttpClient httpClient = HttpClient.newHttpClient()) {
+                  long start = System.currentTimeMillis();
 
+                  Callable<String> openWeather = () -> weatherUtil.getWeather(httpClient, OPEN_WEATHER_URL);
+                  Callable<String> tomorrowWeather = () -> weatherUtil.getWeather(httpClient, TOMORROW_WEATHER_URL);
+                  Callable<String> weatherbitWeather = () -> weatherUtil.getWeather(httpClient, WEATHERBIT_WEATHER_URL);
+
+                  try(ExecutorService executorService = Executors.newFixedThreadPool(POOL_SIZE);) {
+                        Future<String> openWeatherFuture = executorService.submit(openWeather);
+                        Future<String> tomorrowWeatherFuture = executorService.submit(tomorrowWeather);
+                        Future<String> weatherbitWeatherFuture = executorService.submit(weatherbitWeather);
+
+                        String s = openWeatherFuture.get();
+                        String s1 = tomorrowWeatherFuture.get();
+                        String s2 = weatherbitWeatherFuture.get();
+
+                        if (openWeatherFuture.isDone() && tomorrowWeatherFuture.isDone() && weatherbitWeatherFuture.isDone()) {
+                              System.out.println("All futures completed");
+                        }
+
+                        long end = System.currentTimeMillis();
+                        long duration = end - start;
+                        System.out.println("Test 2 asynchronous");
+                        System.out.println("Duration : " + duration);
+                        System.out.println();
+
+                  } catch (InterruptedException | ExecutionException e) {
+                        throw new RuntimeException(e);
+                  }
+            }
       }
 }
