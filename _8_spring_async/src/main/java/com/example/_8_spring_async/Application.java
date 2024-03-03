7c6b55ca9ba4aec9455b8b45f58cfd1513cfb161
AnatolSalau <salovanatoly5@mail.ru>
2024-03-03 18:59:35 +0300

    lesson 8 Spring async

---
+++ _8_spring_async/src/main/java/com/example/_8_spring_async/Application.java
@@ -0,0 +1,13 @@
+package com.example._8_spring_async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+      public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+      }
+
+}
