94905bcece0c1b8884502ca4b4e54597fb73047f
AnatolSalau <salovanatoly5@mail.ru>
2024-03-06 20:59:24 +0300

    lesson 8 Spring async

--- src/main/java/itvdn/_8_scheduled_executor_service/Main.java
+++ src/main/java/itvdn/_8_scheduled_executor_service/Main.java
@@ -1,28 +1,28 @@
 package itvdn._8_scheduled_executor_service;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.*;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 
-
+@Slf4j
 public class Main {
+      private static AtomicInteger count = new AtomicInteger(0);
       public static void main(String[] args) {
             ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 
-            Runnable task2Runnable = () -> System.out.println("Running task2...");
+            Runnable task1 = () -> {
+                  int i = count.incrementAndGet();
+                  log.info("Running task1 - count : " + i);
+            };
 
-            task1();
-            scheduledExecutorService.schedule(task2Runnable, 5, TimeUnit.SECONDS);
-            task3();
+            ScheduledFuture<?> scheduledFuture = scheduledExecutorService.scheduleAtFixedRate(task1, 0, 1, TimeUnit.SECONDS);
+
+
 
             scheduledExecutorService.shutdown();
-      }
-
-      public static void task1() {
-            System.out.println("Running task1...");
-      }
-
-      public static void task3() {
-            System.out.println("Running task3...");
       }
 }
---
+++ src/main/java/itvdn/_8_scheduled_executor_service/TaskAfter5Sec.java
@@ -0,0 +1,29 @@
+package itvdn._8_scheduled_executor_service;
+
+import java.util.concurrent.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+
+public class TaskAfter5Sec {
+      public static void main(String[] args) {
+            ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+
+            Runnable task2Runnable = () -> System.out.println("Running task2...");
+
+            task1();
+            // to delay task to 5 second
+            scheduledExecutorService.schedule(task2Runnable, 5, TimeUnit.SECONDS);
+            task3();
+
+            scheduledExecutorService.shutdown();
+      }
+
+      public static void task1() {
+            System.out.println("Running task1...");
+      }
+
+      public static void task3() {
+            System.out.println("Running task3...");
+      }
+}
