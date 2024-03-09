d987f5b82f55984bad1e8f6d0c0bcef3ad3d67ee
AnatolSalau <salovanatoly5@mail.ru>
2024-03-09 19:36:36 +0300

    lesson 10 ScatherGather init

---
+++ _10_scather_gather_data_service/src/test/java/com/example/_10_scather_gather_data_service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._10_scather_gather_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+      @Test
+      void contextLoads() {
+      }
+
+}
